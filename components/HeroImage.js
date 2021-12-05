e47b45128a764f268b4c3e0b473cae2c07d810e6
Ibrahim Zahir <74900384+ibrahimzahir@users.noreply.github.com>
2021-12-05 13:32:43 +0500

    Carousel

--- components/HeroImage.js
+++ components/HeroImage.js
@@ -1,5 +1,35 @@
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+
 function HeroImage() {
-  return <div>Hero Image</div>;
+  return (
+    <div className="relative">
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showStatus={false}
+        showIndicators={false}
+        showThumbs={false}
+        interval={5000}
+      >
+        <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
+          <img loading="lazy" src="/bml1.png" />
+        </div>
+        <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px] ">
+          <img loading="lazy" src="/bml2.png" />
+        </div>
+        <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
+          <img loading="lazy" src="/bml3.png" />
+        </div>
+        <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
+          <img loading="lazy" src="/bml4.jpg" />
+        </div>
+        <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
+          <img loading="lazy" src="/bml5.jpg" />
+        </div>
+      </Carousel>
+    </div>
+  );
 }
 
 export default HeroImage;
