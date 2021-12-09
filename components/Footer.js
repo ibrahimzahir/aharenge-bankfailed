6e8b3e9185822b9f09cf07dcc7e26cdd2ea431b7
Ibrahim Zahir <74900384+ibrahimzahir@users.noreply.github.com>
2021-12-09 20:35:46 +0500

    footer fixing

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,9 @@
 function Footer() {
   return (
-    <div className=" bg-black py-6">
+    <div className="bg-black py-6">
       <div
-        className="container md:auto pl-24 grid grid-cols-1 sm:grid-cols-6
-     divide-x grid-white space-x-6"
+        className="container md:auto sm:pl-24 grid grid-cols-1 sm:grid-cols-6
+     sm:divide-x sm:grid-white space-x-6 items-center space-y-10"
       >
         <div className="text-gray-400 text-xs text-center">WORK WITH US</div>
         <div className="text-gray-400 text-xs text-center">FIND US</div>
@@ -16,9 +16,12 @@ function Footer() {
           PRIVACY STATEMENT
         </div>
       </div>
-      <div className="divide-y divide-gray-400 px-32 py-4">
-        <div className="container auto pl-32"></div>
-        <div className="text-gray-200 text-sm py-5"> © Aharengge Bank</div>
+      <div className="hidde sm:divide-y sm:divide-gray-400 px-32 py-4">
+        <div className="container auto sm:pl-32 "></div>
+        <div className="text-gray-200 text-sm py-5 px-14">
+          {" "}
+          © Aharengge Bank
+        </div>
       </div>
     </div>
   );
